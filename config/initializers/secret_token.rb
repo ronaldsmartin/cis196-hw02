62b93ea8a69107f4dcba224d75292ce0dcd0998f
Ronald Martin <ronaldmartin@gmail.com>
2013-01-23 11:11:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cis196Hw02::Application.config.secret_token = 'c6861b3b7d9e3ceb592c8450343899be990539fd968f4563009fa7c30d0689ef7786ea2a7f48345bd00b604d01dbc7b662069efa0dd4246f5cc1c6b0b25f5ef1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cis196Hw02::Application.config.session_store :cookie_store, key: '_cis196-hw02_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cis196Hw02::Application.config.session_store :active_record_store
